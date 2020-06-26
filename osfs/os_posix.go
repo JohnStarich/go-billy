44496b3e914e807c9bbeffda81387009fe2f69f0
John Starich <johnstarich@johnstarich.com>
2020-06-26 00:28:14 -0500

    Fix compatibility with js,wasm

--- osfs/os_posix.go
+++ osfs/os_posix.go
@@ -5,21 +5,21 @@ package osfs
 import (
 	"os"
 
-	"golang.org/x/sys/unix"
+	"syscall"
 )
 
 func (f *file) Lock() error {
 	f.m.Lock()
 	defer f.m.Unlock()
 
-	return unix.Flock(int(f.File.Fd()), unix.LOCK_EX)
+	return syscall.Flock(int(f.File.Fd()), syscall.LOCK_EX)
 }
 
 func (f *file) Unlock() error {
 	f.m.Lock()
 	defer f.m.Unlock()
 
-	return unix.Flock(int(f.File.Fd()), unix.LOCK_UN)
+	return syscall.Flock(int(f.File.Fd()), syscall.LOCK_UN)
 }
 
 func rename(from, to string) error {
